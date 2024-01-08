570a49766dea801049f3080a895215ab919dba6f
gauravd03 <gauravdandekar0071@gmail.com>
2024-01-08 12:41:31 +0530

    Print factorial program

---
+++ PrintFactorial.java
@@ -0,0 +1,19 @@
+
+class PrintFactorial{
+
+	public static void main(String args []){
+		int num=3;
+		int fact=1;
+		
+		if(num==0){
+		System.out.println("1");
+		}else{
+			
+		for(int i=1; i<=num; i++){
+			fact=fact*i;
+		}	
+		}
+			System.out.println(fact);
+
+}
+}

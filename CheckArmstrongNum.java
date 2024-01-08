183d13b768b3f1edc27bb6563d1a23f1877d0b0a
gauravd03 <gauravdandekar0071@gmail.com>
2024-01-08 12:11:02 +0530

    armstrong number program

---
+++ CheckArmstrongNum.java
@@ -0,0 +1,22 @@
+ class CheckArmstrongNum{
+	public static void main(String args[]){
+
+		int num=1533;
+ 		int temp=num;
+		int ld=0;
+		int armsnum=0;
+
+		while(temp!=0){
+			ld=temp%10;
+			armsnum=(ld*ld*ld)+armsnum;
+
+			temp=temp/10;
+			}
+		
+		if(armsnum==num){
+		System.out.println("armstrong number");
+		}else{
+		System.out.println("not armstrong number");
+
+		}
+}}

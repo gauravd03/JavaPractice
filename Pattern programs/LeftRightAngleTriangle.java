ce901b948f52007566179f327008c194b30a026c
gauravd03 <gaurauvdandekar0071@gmail.com>
2024-01-31 17:46:52 +0530

    pattern program added

---
+++ Pattern programs/LeftRightAngleTriangle.java
@@ -0,0 +1,21 @@
+class LeftRightAngleTriangle{
+
+	public static void main(String args[]){
+	
+
+	int num=5;
+	for(int i=1; i<=num; i++){
+		for(int j=i; j<num; j++){
+			System.out.print(" ");
+	
+		}
+		
+		for(int k=1; k<=i; k++){
+			System.out.print("*");
+		}
+		
+		System.out.println();
+}
+
+}}
+

7c755912473498da3892401d2514976f84b9577a
Gaurav Dandekar <136999599+gauravd03@users.noreply.github.com>
2024-02-27 15:18:37 +0530

    Update Triangle.java

---
+++ Pattern programs/Triangle.java
@@ -0,0 +1,26 @@
+class Triangle{
+    public static void main(String args []){
+   
+    int n=5;
+    for(int i=1; i<=n; i++){
+        for(int j=i; j<n; j++){
+            System.out.print(" ");
+        }
+       
+        for(int k=1; k<=i; k++){
+        System.out.print("  x");
+        }
+        System.out.println();
+ 
+ 
+ 
+ 
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+}
+}
